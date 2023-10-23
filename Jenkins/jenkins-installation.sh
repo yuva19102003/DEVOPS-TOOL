a6167c32153b90b7b069825045c0b95e59a0bcfa
YUVARAJ K <116803074+yuva19102003@users.noreply.github.com>
2023-10-23 20:07:42 +0530

    Update jenkins-installation.sh

--- Jenkins/jenkins-installation.sh
+++ Jenkins/jenkins-installation.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+# this jenkins installation shell script is for installing jenkins in ubuntu ec2 instances
+
 echo "####################################################################################################################################################################################"
 echo "******* JENKINS INSTALLATION ********"
 echo "####################################################################################################################################################################################"
 echo "***** CHECKING JAVA IS INSTALLED OR NOT, THEN INSTALLING JENKINS *****"
 echo "####################################################################################################################################################################################"
 if command -v java &>/dev/null; then
+# checking java is installed or not 
+# if installed means then install the jenkins
     echo "Java is installed. Version:"
     java -version
     echo "####################################################################################################################################################################################"
@@ -15,11 +19,13 @@ if command -v java &>/dev/null; then
     sudo apt-get update
     sudo apt-get install jenkins -y
 else
+# if java is not installed means then install java first
     echo "Java is not installed. Installing Java..."
     sudo apt-get update
     sudo apt-get install openjdk-11-jre -y
     echo "Java is installed. Version:"
     java -version
+    # now install the jenkins
     echo "####################################################################################################################################################################################"
     echo "JENKINS INSTALLATION STARTS"
     echo "####################################################################################################################################################################################"
@@ -29,7 +35,7 @@ else
     sudo apt-get install jenkins -y
 fi
 echo "####################################################################################################################################################################################"
-
+# checking jenkins is running or not by using systemctl command
 echo "####################################################################################################################################################################################"
 echo "CHECKING JENKINS INSTALLED SUCCESSFULLY OR NOT"
 echo "####################################################################################################################################################################################"
@@ -38,4 +44,8 @@ if systemctl is-active --quiet jenkins; then
 else
     echo "JENKINS NOT INSTALLED AND NOT RUNNING SUCCESSFULLY"
 fi
+echo "####################################################################################################################################################################################"
+echo "JENKINS LOGIN PASSWORD "
+cat /var/jenkins_home/secrets/initialAdminPassword > jenkins-password.txt
+echo "PASSWORD IS STORED IN 'jenkins-password.txt'.."
 echo "####################################################################################################################################################################################"
