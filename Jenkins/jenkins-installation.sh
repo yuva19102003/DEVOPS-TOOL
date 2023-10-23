986f3f54eae8879c285434d1bc1aa9871a547d28
YUVARAJ K <116803074+yuva19102003@users.noreply.github.com>
2023-10-23 20:13:39 +0530

    Update jenkins-installation.sh

--- Jenkins/jenkins-installation.sh
+++ Jenkins/jenkins-installation.sh
@@ -14,9 +14,12 @@ if command -v java &>/dev/null; then
     echo "####################################################################################################################################################################################"
     echo "JENKINS INSTALLATION STARTS"
     echo "####################################################################################################################################################################################"
-    wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-    sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-    sudo apt-get update
+    curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sudo apt-get update -y
     sudo apt-get install jenkins -y
 else
 # if java is not installed means then install java first
@@ -29,9 +32,12 @@ else
     echo "####################################################################################################################################################################################"
     echo "JENKINS INSTALLATION STARTS"
     echo "####################################################################################################################################################################################"
-    wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-    sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-    sudo apt-get update
+    curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sudo apt-get update -y
     sudo apt-get install jenkins -y
 fi
 echo "####################################################################################################################################################################################"
