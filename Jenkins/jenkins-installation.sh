ff55bce4af2c2b8a4b6f428a3bebc6cb6dae85a5
YUVARAJ K <116803074+yuva19102003@users.noreply.github.com>
2023-12-17 21:11:38 +0530

    Update jenkins-installation.sh

--- Jenkins/jenkins-installation.sh
+++ Jenkins/jenkins-installation.sh
@@ -3,6 +3,7 @@
 
 echo "####################################################################################################################################################################################"
 echo "******* JENKINS INSTALLATION ********"
+
 echo "####################################################################################################################################################################################"
 echo "***** CHECKING JAVA IS INSTALLED OR NOT, THEN INSTALLING JENKINS *****"
 echo "####################################################################################################################################################################################"
@@ -11,35 +12,45 @@ if command -v java &>/dev/null; then
 # if installed means then install the jenkins
     echo "Java is installed. Version:"
     java -version
+   
     echo "####################################################################################################################################################################################"
     echo "JENKINS INSTALLATION STARTS"
     echo "####################################################################################################################################################################################"
-    curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    
+    sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
     echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
-    sudo apt-get update -y
-    sudo apt-get install jenkins -y
+    sudo apt-get update
+    sudo apt-get install jenkins
 else
 # if java is not installed means then install java first
     echo "Java is not installed. Installing Java..."
-    sudo apt-get update
-    sudo apt-get install openjdk-11-jre -y
+    
+    sudo apt update
+    sudo apt install fontconfig openjdk-17-jre
+    java -version
+    openjdk version "17.0.8" 2023-07-18
+    OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
+    OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
+    
     echo "Java is installed. Version:"
     java -version
     # now install the jenkins
     echo "####################################################################################################################################################################################"
     echo "JENKINS INSTALLATION STARTS"
     echo "####################################################################################################################################################################################"
-    curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    
+    sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
     echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
-    sudo apt-get update -y
-    sudo apt-get install jenkins -y
+    sudo apt-get update
+    sudo apt-get install jenkins
 fi
+
 echo "####################################################################################################################################################################################"
 # checking jenkins is running or not by using systemctl command
 echo "####################################################################################################################################################################################"
